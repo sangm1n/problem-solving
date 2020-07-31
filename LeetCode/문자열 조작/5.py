cd142691313e3864ae90e48a08c3a728f0511e60
Lee Sang Min <dltkd96als@naver.com>
2020-07-31 14:30:01 +0900

    longest palindrome substring

--- LeetCode/문자열 조작/5.py
+++ LeetCode/문자열 조작/5.py
@@ -7,10 +7,10 @@ Given a string s, find the longest palindromic substring in s. You may assume th
 
 def longestPalindrome(s: str) -> str:
     def expand(left: int, right: int) -> str:
-        while left >= 0 and right <= len(s) and s[left] == s[right - 1]:
+        while left >= 0 and right < len(s) and s[left] == s[right]:
             left -= 1
             right += 1
-        return s[left + 1:right - 1]
+        return s[left + 1:right]
 
     if len(s) < 2 or s == s[::-1]:
         return s
