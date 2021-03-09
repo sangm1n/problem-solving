b31546ee68c1b897b6d7c864218b77ad5c3abfe2
sangm1n <dltkd96als@naver.com>
2021-03-09 19:11:08 +0900

    Update: 2021-03-09

--- BOJ/2606.py
+++ BOJ/2606.py
@@ -4,31 +4,35 @@ github : https://github.com/sangm1n
 e-mail : dltkd96als@naver.com
 
 title : 바이러스
-description : DFS
+description : BFS
 """
 
+from collections import deque
 
-def dfs(n):
-    global count
-    visited[n] = True
-    count += 1
 
-    for v in network[n]:
-        if not visited[v]:
-            visited[v] = True
-            dfs(v)
+def bfs(start):
+    q = deque()
+    q.append(start)
+    visited[start] = True
+
+    while q:
+        v = q.popleft()
+
+        for i in graph[v]:
+            if not visited[i]:
+                visited[i] = True
+                q.append(i)
 
 
 N = int(input())
 M = int(input())
-network = [[] for _ in range(N+1)]
-visited = [False] * (N+1)
 
+graph = [[] for _ in range(N+1)]
+visited = [False] * (N+1)
 for _ in range(M):
     a, b = map(int, input().split())
-    network[a].append(b)
-    network[b].append(a)
+    graph[a].append(b)
+    graph[b].append(a)
 
-count = 0
-dfs(1)
-print(count - 1)
+bfs(1)
+print(visited[1:].count(True) - 1)
