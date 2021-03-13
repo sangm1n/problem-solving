e8a09a0dc989a71b91e3286f4157d3bf11dd07e2
sangm1n <dltkd96als@naver.com>
2021-03-13 15:16:18 +0900

    Update: 2021-03-13

--- BOJ/7569.py
+++ BOJ/7569.py
@@ -10,46 +10,41 @@ description : BFS
 from collections import deque
 
 
-def bfs():
-    global q
+def bfs(q):
+    dx = [-1, 1, 0, 0, 0, 0]
+    dy = [0, 0, -1, 1, 0, 0]
+    dz = [0, 0, 0, 0, 1, -1]
 
-    result = 0
+    count = 0
     while q:
-        x, y, z = q.popleft()
+        z, x, y = q.popleft()
 
         for i in range(6):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            nz = z + dz[i]
+            nx, ny, nz = x + dx[i], y + dy[i], z + dz[i]
 
-            if 0 <= nx < H and 0 <= ny < N and 0 <= nz < M and box[nx][ny][nz] == 0:
-                box[nx][ny][nz] = box[x][y][z] + 1
-                result = max(result, box[nx][ny][nz] - 1)
-                q.append([nx, ny, nz])
+            if 0 <= nx < N and 0 <= ny < M and 0 <= nz < H and boxes[nz][nx][ny] == 0:
+                q.append((nz, nx, ny))
+                boxes[nz][nx][ny] = boxes[z][x][y] + 1
+                count = max(count, boxes[nz][nx][ny] - 1)
 
-    return result
+    return count
 
 
 M, N, H = map(int, input().split())
-box = [[list(map(int, input().split())) for _ in range(N)] for _ in range(H)]
-
-dx = [-1, 1, 0, 0, 0, 0]
-dy = [0, 0, -1, 1, 0, 0]
-dz = [0, 0, 0, 0, -1, 1]
+boxes = [[list(map(int, input().split())) for _ in range(N)] for _ in range(H)]
 
 q = deque()
 for i in range(H):
     for j in range(N):
         for k in range(M):
-            if box[i][j][k] == 1:
-                q.append([i, j, k])
+            if boxes[i][j][k] == 1:
+                q.append((i, j, k))
 
-count = bfs()
-
+result = bfs(q)
 for i in range(H):
     for j in range(N):
         for k in range(M):
-            if box[i][j][k] == 0:
+            if boxes[i][j][k] == 0:
                 print(-1)
                 exit()
-print(count)
+print(result)
