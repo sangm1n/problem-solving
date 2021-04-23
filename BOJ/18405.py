5b9e35128321c46764be5e8e50f6ba30772e955d
sangm1n <dltkd96als@naver.com>
2021-04-23 23:18:21 +0900

    Update: 2021-04-23

--- BOJ/18405.py
+++ BOJ/18405.py
@@ -7,41 +7,39 @@ title : 경쟁적 전염
 description : BFS
 """
 
-import sys
 from collections import deque
-input = sys.stdin.readline
-
-N, K = map(int, input().split())
-grid = [list(map(int, input().split())) for _ in range(N)]
-S, X, Y = map(int, input().split())
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-tmp = []
-for i in range(N):
-    for j in range(N):
-        if grid[i][j] != 0:
-            tmp.append((grid[i][j], 0, i, j))
-
-tmp.sort()
-q = deque(tmp)
 
 
-def bfs(q):
+def spread(q, arr, S):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
     while q:
-        virus, time, x, y = q.popleft()
+        virus, x, y, time = q.popleft()
+
         if time == S:
             break
 
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+            nx, ny = x + dx[i], y + dy[i]
 
-            if 0 <= nx < N and 0 <= ny < N and grid[nx][ny] == 0:
-                grid[nx][ny] = virus
-                q.append((grid[nx][ny], time + 1, nx, ny))
+            if 0 <= nx < len(arr) and 0 <= ny < len(arr) and arr[nx][ny] == 0:
+                arr[nx][ny] = virus
+                q.append((arr[nx][ny], nx, ny, time + 1))
 
 
-bfs(q)
-print(grid[X-1][Y-1])
+N, K = map(int, input().split())
+cylinder = [list(map(int, input().split())) for _ in range(N)]
+S, X, Y = map(int, input().split())
+
+tmp = []
+for i in range(N):
+    for j in range(N):
+        if cylinder[i][j] != 0:
+            tmp.append((cylinder[i][j], i, j, 0))
+
+tmp.sort()
+q = deque(tmp)
+
+spread(q, cylinder, S)
+print(cylinder[X-1][Y-1])
