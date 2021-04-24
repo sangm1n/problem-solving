0072cabc18f6c2ac6ecc761f680b13eaa5a5101c
sangm1n <dltkd96als@naver.com>
2021-04-24 22:45:57 +0900

    Update: 2021-04-24

--- CodingTest/9-5.py
+++ CodingTest/9-5.py
@@ -4,49 +4,40 @@ github : https://github.com/sangm1n
 e-mail : dltkd96als@naver.com
 
 title : 전보
-description : 어떤 나라에는 N개의 도시가 있다. 그리고 각 도시는 보내고자 하는 메시지가 있는 경우, 다른 도시로 전보를 보내 해당 메시지를 전송할 수 있다.
-하지만 X라는 도시에서 Y라는 도시로 전보를 보내고자 한다면, 도시 X에서 Y로 향하는 통로가 설치되어 있어야 한다.
-예를 들어 X에서 Y로 향하는 통로는 있지만, Y에서 X로 향하는 통로가 없다면 Y는 X로 메시지를 보낼 수 없다.
-또한 통로를 거쳐 메시지를 보낼 때는 일정 시간이 소요된다.
-어느 날 C라는 도시에서 위급 상황이 발생했다. 그래서 최대한 많은 도시로 메시지를 보내고자 한다.
-메시지는 도시 C에서 출발하여 각 도시 사이에 설치된 통로를 거쳐, 최대한 많이 퍼져나갈 것이다.
-각 도시의 번호와 통로가 설치되어 있는 정보가 주어졌을 때, 도시 C에서 보낸 메시지를 받게 되는 도시의 개수는 총 몇 개이며
-도시들이 모두 메시지를 받는 데까지 걸리는 시간은 얼마인지 계산하는 프로그램을 작성하시오.
+description : Dijkstra Algorithm
 """
 
 import heapq
 
 
-def dijkstra(start, distance):
+def dijkstra(start, distance, graph):
     q = []
     heapq.heappush(q, (0, start))
-    distance[start] = 0
 
     while q:
-        cost, now = heapq.heappop(q)
+        dist, now = heapq.heappop(q)
 
-        if distance[now] >= cost:
-            for idx, c in graph[now]:
-                new_cost = cost + c
-
-                if new_cost < distance[idx]:
-                    heapq.heappush(q, (new_cost, idx))
-                    distance[idx] = new_cost
+        if distance[now] >= dist:
+            for goal, cost in graph[now]:
+                new_dist = dist + cost
+                if distance[goal] > new_dist:
+                    distance[goal] = new_dist
+                    heapq.heappush(q, (new_dist, goal))
 
 
 N, M, C = map(int, input().split())
 graph = [[] for _ in range(N+1)]
-distance = [int(1e9) for _ in range(N+1)]
+for i in range(M):
+    X, Y, Z = map(int, input().split())
+    graph[X].append((Y, Z))
 
-for _ in range(M):
-    x, y, z = map(int, input().split())
-    graph[x].append((y, z))
+distance = [int(1e9)] * (N+1)
+dijkstra(C, distance, graph)
 
-dijkstra(C, distance)
 count, result = 0, 0
-for d in distance:
-    if 0 < d < int(1e9):
+for i in range(1, N+1):
+    if distance[i] < int(1e9):
         count += 1
-        result = max(result, d)
+        result = max(result, distance[i])
 
 print(count, result)
